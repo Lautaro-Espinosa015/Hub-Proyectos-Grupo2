1b6a20c79cc2c667b4d57076d212cf97dea05032
EnderJack379 <leonelcussi9@gmail.com>
2025-10-16 18:45:51 -0300

    Arreglo de carga fallida de imagenes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/Css/HubStyles.css'; // Importamos los estilos globales
+import './assets/Css/HubStyles.css'; 
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
